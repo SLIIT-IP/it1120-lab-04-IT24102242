8e93224b16dfc506d6d63e09f8d6ea234eae67ad
IT24102242 <it24102242@my.sliit.lk>
2024-08-14 16:54:56 +0530

    Add files via upload

---
+++ Q1/IT24102242Lab4Q1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class IT24102242Lab4Q1
+{
+	public static void main (String[] args)
+	{
+		Scanner num=new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int num1=num.nextInt();
+		
+		if (num1>0)
+			{ System.out.print("The number is: positive"); }
+		else if (num1<0)
+			{ System.out.print("The number is: negative"); }
+		else
+			{ System.out.print("The number is: zero "); }
+	}
+}	
+			
+		
+			
+		
+	
+		
+			
+		
+		
+		
+		
+	
